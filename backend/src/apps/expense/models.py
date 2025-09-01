93ccdbe413590a26da9110df7d3c200d330dca74
Elis Kandel <alishkandel2062@gmail.com>
2025-09-02 02:03:19 +0545

    docker configuration

--- backend/src/apps/expense/models.py
+++ backend/src/apps/expense/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.utils import timezone  # ✅ correct import
+
+
 
 User = get_user_model()
 
